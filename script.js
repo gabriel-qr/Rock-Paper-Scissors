e868ef916b399ec58d9352ce0b54398282993603
gabriel-qr <gabriel.qr.dev@gmail.com>
2025-09-16 08:25:49 -0300

    feat: implement getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('teste')
+const choices = ['rock', 'paper', 'scissors']
+
+ function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)];
+ }
+
+ console.log(getComputerChoice())

14b69a55c0774e5ff16b7b49938a8853642ce146
gabriel-qr <gabriel.qr.dev@gmail.com>
2025-09-16 08:54:28 -0300

    feat: implement playRound function

--- script.js
+++ script.js
@@ -8,4 +8,35 @@ const choices = ['rock', 'paper', 'scissors']
     return prompt('Rock, paper, or scissors?')
  }
 
- console.log(getHumanChoice())
+
+let humanScore = 0
+let computerScore = 0
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+/* paper beats rock
+rock beats scissors
+scissors beats paper */
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+   if (computerChoice === humanChoice){
+    console.log('It was a draw!')
+    console.log(`The score is: You: ${humanScore} | Computer: ${computerScore}`)
+   } else if (
+        (humanChoice === 'paper' && computerChoice === 'rocks') ||
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        console.log(`You win! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`)
+        humanScore ++
+        console.log(`The score is: You: ${humanScore} | Computer: ${computerScore}`)
+    } else {
+        console.log(`You loose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`)
+        computerScore ++
+        console.log(`The score is: You: ${humanScore} | Computer: ${computerScore}`)
+    }
+}
+
+playRound(humanSelection, computerSelection)
